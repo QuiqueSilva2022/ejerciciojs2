8111a10a3c3d44699efe3a6aea8003731d01d29f
QuiqueSilva2022 <infotiempoactual@gmail.com>
2022-11-27 22:05:26 -0300

    Ejercicio js continente

---
+++ js/scrypt.js
@@ -0,0 +1,7 @@
+let pais = prompt(" cual es tu pais ");
+let continente = prompt(" cual es tu co ntinente ");
+let antiguedad = prompt(" Cual es el año ");
+let pais_y_continente = pais + continente + antiguedad;
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
